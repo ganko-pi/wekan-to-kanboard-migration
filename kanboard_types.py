ef369dd0ab6b7f2d808ba62ef86a65635b84612b
Ganko Pi <ganko-pi@outlook.de>
2025-03-02 20:37:41 +0100

    feat: create Kanboard project if it does not already exist

---
+++ kanboard_types.py
@@ -0,0 +1,20 @@
+from typing import TypedDict
+
+class Project(TypedDict):
+    class Url(TypedDict):
+        board: str
+        calendar: str
+        list: str
+
+    id: str
+    name: str
+    is_active: str
+    token: str
+    last_modified: str
+    is_public: str
+    is_private: str
+    default_swimlane: str
+    show_default_swimlane: str
+    description: str
+    identifier: str
+    url: Url
--- wekan_to_kanboard_migration.py
+++ wekan_to_kanboard_migration.py
@@ -1,5 +1,6 @@
 import json
 import kanboard
+import kanboard_types
 import os
 import wekan_types
 from dotenv import load_dotenv
@@ -19,9 +20,22 @@ def migrate() -> None:
 def migrate_wekan_board(kanboard_client: kanboard.Client, json_file_path: str) -> None:
     wekan_board: wekan_types.WekanBoard = load_json(json_file_path)
 
+    wekan_board_title = wekan_board['title']
+
+    kanboard_project = create_kanboard_project(kanboard_client, wekan_board_title)
+
 def load_json(json_file_path: str) -> any:
     with open(json_file_path, 'r') as file:
         return json.load(file)
+
+def create_kanboard_project(kanboard_client: kanboard.Client, project_name: str) -> kanboard_types.Project:
+    project = kanboard_client.get_project_by_name(name=project_name)
+    if project is not False:
+        return project
+
+    project_id = kanboard_client.create_project(name=project_name)
+    project = kanboard_client.get_project_by_id(project_id=project_id)
+    return project
 
 def main() -> None:
     load_dotenv()
