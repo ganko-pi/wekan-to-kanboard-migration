ada9d2d6c258b2cf9f0bbb0a830fd405672d5648
Ganko Pi <ganko-pi@outlook.de>
2025-03-02 20:25:36 +0100

    refactor: add new function migrate

--- wekan_to_kanboard_migration.py
+++ wekan_to_kanboard_migration.py
@@ -2,12 +2,15 @@ import kanboard
 import os
 from dotenv import load_dotenv
 
-def main():
-    load_dotenv()
-
+def migrate() -> None:
     kanboard_api_token = os.getenv('KANBOARD_API_TOKEN')
 
-    kb = kanboard.Client('http://localhost/kanboard/jsonrpc.php', 'jsonrpc', kanboard_api_token, 'X-API-Auth')
+    kanboard_client = kanboard.Client('http://localhost/kanboard/jsonrpc.php', 'jsonrpc', kanboard_api_token, 'X-API-Auth')
+
+def main() -> None:
+    load_dotenv()
+
+    migrate()
 
 if __name__ == '__main__':
     main()
