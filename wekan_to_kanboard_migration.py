c104cf918cf9e14a481067d26e6052d39e2ab718
Ganko Pi <ganko-pi@outlook.de>
2025-03-02 20:30:16 +0100

    feat: read Wekan boards from JSON files

--- wekan_to_kanboard_migration.py
+++ wekan_to_kanboard_migration.py
@@ -1,11 +1,22 @@
+import json
 import kanboard
 import os
 from dotenv import load_dotenv
 
 def migrate() -> None:
     kanboard_api_token = os.getenv('KANBOARD_API_TOKEN')
+    input_directory = os.getenv('INPUT_DIRECTORY')
 
     kanboard_client = kanboard.Client('http://localhost/kanboard/jsonrpc.php', 'jsonrpc', kanboard_api_token, 'X-API-Auth')
+
+    for file in os.listdir(input_directory):
+        if not file.endswith('.json'):
+            continue
+
+        migrate_wekan_board(kanboard_client, os.path.join(input_directory, file))
+
+def migrate_wekan_board(kanboard_client: kanboard.Client, json_file_path: str) -> None:
+    pass
 
 def main() -> None:
     load_dotenv()
